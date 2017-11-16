933e04fa47a5720a25fca901c5794c24e032df78
Michelle Cinello <m.cinello@gmail.com>
2017-11-15 21:49:26 -0500

    set up event handler for button

---
+++ cats.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var button = document.querySelector('button');
+
+  button.addEventListener('click', function(e) {
+    console.log("kitteh");
+  });
+});
