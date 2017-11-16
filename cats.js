a4d28cabfaf32c63ed6066fe0753d35553cde796
Michelle Cinello <m.cinello@gmail.com>
2017-11-15 22:04:48 -0500

    kitties has been summoned.

--- cats.js
+++ cats.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
   var button = document.querySelector('button');
+  var catBox1 = document.getElementById('cat1')
 
   button.addEventListener('click', function(e) {
     $.ajax({
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET',
       dataType: 'json'
+    })
+    .done(function(data){
+      var imgTag = document.createElement('img');
+
+      for (var i = 0; i < data.cats.length; i++) {
+        imgTag.src = data.cats[i].photo;
+        imgTag.alt = "Photo of " + data.cats[i].name
+        catBox1.append(imgTag)
+      };
     })
   });
 });
